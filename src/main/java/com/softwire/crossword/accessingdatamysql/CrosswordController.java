2d54fc3ddac32f714ab311788e9fd860af4ad73c
JoelWee <Joel.Wee@softwire.com>
2020-05-01 14:33:49 +0100

    Display crossword list

--- src/main/java/com/softwire/crossword/accessingdatamysql/CrosswordController.java
+++ src/main/java/com/softwire/crossword/accessingdatamysql/CrosswordController.java
@@ -2,18 +2,20 @@ package com.softwire.crossword.accessingdatamysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @Controller
-@RequestMapping(path = "")
 public class CrosswordController {
     @Autowired
     private CrosswordRepository crosswordRepository;
 
-    @GetMapping(path = "/")
-    public String view() {
+    @GetMapping(path = "/crosswords")
+    public String crosswords(Model model){
+        var crosswords = crosswordRepository.findAll();
+        model.addAttribute("crosswords", crosswords);
         return "crosswords";
     }
 
